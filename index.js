7933f0d7049960b66c906c37f7185043480217c1
slowrunr <mungaslov.nft@gmail.com>
2023-10-23 00:14:35 +0900

    rearranged scripts in index.html

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const controller = new Controller();
-
+const app = new Controller();
 import Controller from "./controller.js";
 
-const model = new Model({
-  onPostsChanged: handleModelPostsChanged,
-});
+// const model = new Model({
+//   onPostsChanged: handleModelPostsChanged,
+// });
 
-import Model from "./model.js";
+// import Model from "./model.js";
 
-const view = new View();
-import View from "./view.js";
+// const view = new View();
+// import View from "./view.js";
 
-view.renderPosts(model.getPosts());
+// view.renderPosts(model.getPosts());
 
-model.addPost("1", "Hi");
+// model.addPost("1", "Hi");
 
-view.renderPosts(model.getPosts());
+// view.renderPosts(model.getPosts());
 
-// model.addPost("2", "bye");
+// // model.addPost("2", "bye");
 
-function handleModelPostsChanged(posts) {
-  console.log(posts);
-}
+// function handleModelPostsChanged(posts) {
+//   console.log(posts);
+// }
