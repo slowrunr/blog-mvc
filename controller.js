cc3c53af6794145cf1ded4e001f0120550a4dee5
slowrunr <mungaslov.nft@gmail.com>
2023-10-23 22:49:44 +0900

    followed 10. lesson

--- controller.js
+++ controller.js
@@ -2,25 +2,31 @@ import { Model } from "./model.js"; // change import view from 'Model' to '{ Mod
 import { View } from "./view.js";
 
 export class Controller {
-    constructor() {
-        this.model = new Model({
-            onPostsChanged: this.handleModelPostsChanged,
-        });
+  constructor() {
+    this.model = new Model({
+      onPostsChanged: this.handleModelPostsChanged,
+    });
 
-        this.view = new View();
-    }
+    this.view = new View({
+      onNewPost: this.handleViewNewPost,
+    });
+  }
 
-    // intro init function with linkage test
-    init = () => {
-        // TEST AND TBS - REMOVE IN PRODUCTION
-        console.log("HELLO! INIT FUNCTION CARRIED OUT SUCCESFULLY");
+  // intro init function with linkage test
+  init = () => {
+    // TEST AND TBS - REMOVE IN PRODUCTION
+    console.log("HELLO! INIT FUNCTION CARRIED OUT SUCCESFULLY");
 
-        // TEST AND TBS - REMOVE IN PRODUCTION
-        this.model.checkModuleLinkage();
-        this.view.checkModuleLinkage();
-    };
+    // TEST AND TBS - REMOVE IN PRODUCTION
+    this.model.checkModuleLinkage();
+    this.view.checkModuleLinkage();
+  };
 
-    handleModelPostsChanged = (posts) => {
-        this.view.renderPosts(posts);
-    };
+  handleModelPostsChanged = (posts) => {
+    this.view.renderPosts(posts);
+  };
+
+  handleViewNewPost = (title, description) => {
+    this.model.addPost(title, description);
+  };
 }
--- view.js
+++ view.js
@@ -1,24 +1,40 @@
 // Removed 'default' here:
 export class View {
-    constructor() {
-        this.postsNode = document.getElementById("posts");
-    }
+  constructor({ onNewPost }) {
+    this.postsNode = document.getElementById("#posts");
+    this.titleInputNode = document.getElementById("titleInput");
+    this.textInputNode = document.getElementById("textInput");
+    this.addPostBtnNode = document.getElementById("postBtn");
 
-    // TEST AND TBS - REMOVE IN PRODUCTION
-    checkModuleLinkage = () => {
-        console.log("HELLO, View module is connected!");
-    };
+    this.onNewPost = onNewPost;
 
-    renderPosts(posts) {
-        this.postsNode.innerHTML = "";
+    this.addPostBtnNode.addEventListener("click", this.handleBtnClick);
+  }
 
-        posts.forEach((post) => {
-            this.postsNode.innerHTML += `
+  // TEST AND TBS - REMOVE IN PRODUCTION
+  checkModuleLinkage = () => {
+    console.log("HELLO, View module is connected!");
+  };
+
+  renderPosts(posts) {
+    this.postsNode.innerHTML = "";
+
+    posts.forEach((post) => {
+      this.postsNode.innerHTML += `
       <li class="post">
         <p class="post__title">${post.title}</p>
         <p class="post__description">${post.description}</p>
       </li>
      `;
-        });
-    }
+    });
+  }
+
+  handleBtnClick = () => {
+    const title = this.titleInputNode.value;
+    const description = this.textInputNode.value;
+
+    this.onNewPost(title, description);
+
+    console.log(title, description);
+  };
 }
